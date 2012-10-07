685a202b40795c5a621673ba6e3a9d44f1e496a1
Simon Cropp <simon.cropp@gmail.com>
2012-10-07 16:09:20 +1100

    fix nuget install

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("PropertyChanged")]
 [assembly: AssemblyProduct("PropertyChanged")]
-[assembly: AssemblyVersion("1.29.0.0")] 
-[assembly: AssemblyFileVersion("1.29.0.0")]
+[assembly: AssemblyVersion("1.30.0.0")] 
+[assembly: AssemblyFileVersion("1.30.0.0")]
  
