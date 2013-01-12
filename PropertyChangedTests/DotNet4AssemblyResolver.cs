1b30b2a62c4d2ef469b275afec08a38f0596dbea
Simon Cropp <simon.cropp@gmail.com>
2013-01-13 08:50:23 +1100

    more tests

---
+++ PropertyChangedTests/DotNet4AssemblyResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using Mono.Cecil;
+
+public class DotNet4AssemblyResolver : IAssemblyResolver
+{
+    public AssemblyDefinition Resolve(AssemblyNameReference name)
+    {
+        return AssemblyDefinition.ReadAssembly(name.Name + ".dll");
+
+    }
+
+    public AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public AssemblyDefinition Resolve(string fullName)
+    {
+        if (fullName == "System")
+        {
+            var codeBase = typeof(Debug).Assembly.CodeBase.Replace("file:///", "");
+            return AssemblyDefinition.ReadAssembly(codeBase);
+        }
+        else
+        {
+            var codeBase = typeof(string).Assembly.CodeBase.Replace("file:///", "");
+            return AssemblyDefinition.ReadAssembly(codeBase);
+        }
+
+    }
+
+    public AssemblyDefinition Resolve(string fullName, ReaderParameters parameters)
+    {
+        throw new NotImplementedException();
+    }
+}
