e3dcf031b0265a2e4c3ab3d2b0302ee7e357a6a3
Simon Cropp <simon.cropp@gmail.com>
2013-03-24 16:45:20 +1100

    fix project level hack removal

--- NuGet/install.ps1
+++ NuGet/install.ps1
@@ -3,22 +3,19 @@
 
 function RemoveForceProjectLevelHack($project)
 {
-	if (Test-Path "content/Fody_ToBeDeleted.txt")
+	Foreach ($item in $project.ProjectItems) 
 	{
-		$itemToRemove = $project.ProjectItems.Item("Fody_ToBeDeleted.txt")	
-		$itemToRemove.Delete()
-	}	
+		if ($item.Name -eq "Fody_ToBeDeleted.txt")
+		{
+			$item.Delete()
+		}
+	}
 }
 
 function Update-FodyConfig($addinName, $project)
 {
 	
     $fodyWeaversPath = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($project.FullName), "FodyWeavers.xml")
-
-    if (!(Test-Path ($fodyWeaversPath)))
-    {
-        Throw "Could not find FodyWeavers.xml in this project. Please enable Fody for this projet http://visualstudiogallery.msdn.microsoft.com/074a2a26-d034-46f1-8fe1-0da97265eb7a"
-    }   
 
 	$FodyLastProjectPath = $env:FodyLastProjectPath
 	$FodyLastWeaverName = $env:FodyLastWeaverName
