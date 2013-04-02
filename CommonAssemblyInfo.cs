03df45c938d4356a31ff6d91c73dce97a215c566
Simon Cropp <simon.cropp@gmail.com>
2013-04-03 00:03:55 +1100

    version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("PropertyChanged")]
 [assembly: AssemblyProduct("PropertyChanged")]
-[assembly: AssemblyVersion("1.36.2.2")] 
-[assembly: AssemblyFileVersion("1.36.2.2")]
+[assembly: AssemblyVersion("1.36.2.3")] 
+[assembly: AssemblyFileVersion("1.36.2.3")]
  
