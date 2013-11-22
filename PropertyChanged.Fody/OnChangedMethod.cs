95665bab96b3e3e8354d383dedb992057d4ed129
Damian Reeves <damreev@gmail.com>
2013-11-22 17:21:36 -0500

    add change

---
+++ PropertyChanged.Fody/OnChangedMethod.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Cecil;
+
+public enum OnChangedTypes
+{
+    NoArg,
+    BeforeAfter,
+}
+
+public class OnChangedMethod
+{
+    public MethodReference MethodReference;
+    public OnChangedTypes OnChangedType;
+}
