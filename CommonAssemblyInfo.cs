2e76d36e0d096a9e97f8320dd71162472445bd7d
Simon Cropp <simon.cropp@gmail.com>
2013-12-17 08:24:55 +1100

    make it a dev nuget

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("PropertyChanged")]
 [assembly: AssemblyProduct("PropertyChanged")]
-[assembly: AssemblyVersion("1.43.0")] 
-[assembly: AssemblyFileVersion("1.43.0")]
+[assembly: AssemblyVersion("1.43.1")] 
+[assembly: AssemblyFileVersion("1.43.1")]
  
