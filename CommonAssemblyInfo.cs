195956de122bcb0e5965423ab9969e811036b163
Simon Cropp <simon.cropp@gmail.com>
2014-03-02 11:22:42 +1100

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyTitle("PropertyChanged")]
 [assembly: AssemblyProduct("PropertyChanged")]
-[assembly: AssemblyVersion("1.46.0")] 
-[assembly: AssemblyFileVersion("1.46.0")]
+[assembly: AssemblyVersion("1.46.1")] 
+[assembly: AssemblyFileVersion("1.46.1")]
  
