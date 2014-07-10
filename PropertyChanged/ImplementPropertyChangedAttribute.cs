d4ad2da411d226148b55ba7ebc83635b8c89e8fc
Bruno Juchli <bruno.juchli@innocreative.ch>
2014-07-10 09:40:01 +0200

    Fixed XML comment formatting
    tested with VS2013. IntelliSense does not show hyperlink (not even the text), as it doesn't support <see href=""/>.
    This way it works for any tool.

--- PropertyChanged/ImplementPropertyChangedAttribute.cs
+++ PropertyChanged/ImplementPropertyChangedAttribute.cs
@@ -4,9 +4,13 @@ namespace PropertyChanged
 {
     /// <summary>
     /// Specifies that PropertyChanged Notification will be added to a class.
+    /// <para>
     /// PropertyChanged.Fody will weave the <c>INotifyPropertyChanged</c> interface and implementation into the class.
     /// When the value of a property changes, the PropertyChanged notification will be raised automatically
-    /// See https://github.com/Fody/PropertyChanged for more information.
+    /// </para>
+    /// <para>
+    /// see https://github.com/Fody/PropertyChanged <see href="https://github.com/Fody/PropertyChanged">(link)</see> for more information.
+    /// </para>
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public class ImplementPropertyChangedAttribute : Attribute
