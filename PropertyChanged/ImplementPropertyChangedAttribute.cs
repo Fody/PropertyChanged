56ea75cb666d5ef7bcb6092218ea9ff370e25783
Simon Cropp <simon.cropp@gmail.com>
2014-07-28 17:05:09 +1000

    fix doco

--- PropertyChanged/ImplementPropertyChangedAttribute.cs
+++ PropertyChanged/ImplementPropertyChangedAttribute.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace PropertyChanged
 {
+
     /// <summary>
     /// Specifies that PropertyChanged Notification will be added to a class.
     /// <para>
-    /// PropertyChanged.Fody will weave the <c>INotifyPropertyChanged</c> interface and implementation into the class.
+    /// PropertyChanged.Fody will weave the <see cref="INotifyPropertyChanged"/> interface and implementation into the class.
     /// When the value of a property changes, the PropertyChanged notification will be raised automatically
     /// </para>
     /// <para>
