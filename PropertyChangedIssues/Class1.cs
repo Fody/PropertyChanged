3403a9237a0d3e92ae3d78e9ea5b005dba5c8a39
MendelMonteiro <mendel.monteiro@gmail.com>
2014-12-13 11:59:52 +0100

    Add issues project with LinqToSQL bugs unit tested

---
+++ PropertyChangedIssues/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace PropertyChangedIssues
+{
+    partial class Class1
+    {
+        public int CalledProperty1OnChangedMethod { get; private set; }
+        public int CalledIdOnChangedMethod { get; private set; }
+
+        partial void OnProperty1Changed()
+        {
+            CalledProperty1OnChangedMethod++;
+        }
+
+        partial void OnIdChanged()
+        {
+            CalledIdOnChangedMethod++;
+        }
+    }
+}
---
+++ PropertyChangedIssues/LinqToSqlTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace PropertyChangedIssues
+{
+    [TestFixture]
+    public class LinqToSqlTests
+    {
+        [Test]
+        public void ShouldRaisesNotifyPropertyChangedOnce()
+        {
+            Class1 c = new Class1();
+
+            int count = 0;
+            c.PropertyChanged += (sender, args) => count++;
+            c.Property1 = "hello";
+
+            Assert.AreEqual(1, count);
+        }
+
+        [Test]
+        public void ShouldCallPartialMethodOnce()
+        {
+            Class1 c = new Class1();
+            
+            c.Property1 = "hello";
+            c.Id = "id";
+
+            Assert.AreEqual(1, c.CalledProperty1OnChangedMethod);
+            Assert.AreEqual(1, c.CalledIdOnChangedMethod);
+        }
+    }
+}
