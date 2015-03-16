b26e1e9272b18507257a99d76622ed79b66996a4
Cameron MacFarland <distantcam@gmail.com>
2015-03-16 17:37:43 +0800

    Removing more legacy powershell

--- NuGet/install.ps1
+++ NuGet/install.ps1
@@ -1,18 +1,6 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
 
-function RemoveForceProjectLevelHack($project)
-{
-    Write-Host "RemoveForceProjectLevelHack" 
-	Foreach ($item in $project.ProjectItems) 
-	{
-		if ($item.Name -eq "Fody_ToBeDeleted.txt")
-		{
-			$item.Delete()
-		}
-	}
-}
-
 function Fix-ReferencesCopyLocal($package, $project)
 {
     Write-Host "Fix-ReferencesCopyLocal $($package.Id)"
@@ -30,18 +18,4 @@ function Fix-ReferencesCopyLocal($package, $project)
     }
 }
 
-function Set-NugetPackageRefAsDevelopmentDependency($package, $project)
-{
-	Write-Host "Set-NugetPackageRefAsDevelopmentDependency" 
-    $packagesconfigPath = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($project.FullName), "packages.config")
-	$packagesconfig = [xml](get-content $packagesconfigPath)
-	$packagenode = $packagesconfig.SelectSingleNode("//package[@id=`'$($package.id)`']")
-	$packagenode.SetAttribute('developmentDependency','true')
-	$packagesconfig.Save($packagesconfigPath)
-}
-
-RemoveForceProjectLevelHack $project
-
 Fix-ReferencesCopyLocal $package $project
-
-Set-NugetPackageRefAsDevelopmentDependency $package $project
