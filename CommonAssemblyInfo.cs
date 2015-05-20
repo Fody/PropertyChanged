95fbf5a7c0e4a1ae9a2bb21c689e10479ccc527c
Simon Cropp <simon.cropp@gmail.com>
2015-05-20 14:58:12 +1000

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,6 +4,6 @@
 [assembly: AssemblyProduct("PropertyChanged")]
 [assembly: AssemblyCompany("Simon Cropp and Contributors")]
 [assembly: AssemblyDescription("Fody add-in for injecting INotifyPropertyChanged code into properties.")]
-[assembly: AssemblyVersion("1.49.1")]
-[assembly: AssemblyFileVersion("1.49.1")]
+[assembly: AssemblyVersion("1.50.0")]
+[assembly: AssemblyFileVersion("1.50.0")]
  
