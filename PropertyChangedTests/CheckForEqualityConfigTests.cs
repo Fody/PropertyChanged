4116fc58551f6f205ee7eff4e1b2e6128d69c2bd
Mihi <madcowdevelopment@googlemail.com>
2015-07-30 14:44:40 +0800

    added unit tests for CheckForEqualityConfig

---
+++ PropertyChangedTests/CheckForEqualityConfigTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Linq;
+using NUnit.Framework;
+
+[TestFixture]
+public class CheckForEqualityConfigTests
+{
+    [Test]
+    public void False()
+    {
+        var xElement = XElement.Parse("<PropertyChanged CheckForEquality='false'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveCheckForEqualityConfig();
+        Assert.IsFalse(moduleWeaver.CheckForEquality);
+    }
+
+    [Test]
+    public void True()
+    {
+        var xElement = XElement.Parse("<PropertyChanged CheckForEquality='true'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveCheckForEqualityConfig();
+        Assert.IsTrue(moduleWeaver.CheckForEquality);
+    }
+
+    [Test]
+    public void Default()
+    {
+        var moduleWeaver = new ModuleWeaver();
+        moduleWeaver.ResolveCheckForEqualityConfig();
+        Assert.IsTrue(moduleWeaver.CheckForEquality);
+    }
+}
