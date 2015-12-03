b209cc42c0b381e10a8541fac8cfd7114ddbb3da
TDenis <dtimukhin@yandex.ru>
2015-12-03 22:58:09 +0300

    Add OnPropertyChanged attribute

---
+++ PropertyChanged/OnPropertyChangedAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PropertyChanged
+{
+    /// <summary>
+    /// Injects this method to be called when a dependent property is set.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class OnPropertyChangedAttribute : Attribute
+    {
+        ///<summary>
+        /// Initializes a new instance of <see cref="OnPropertyChangedAttribute"/>.
+        ///</summary>
+        ///<param name="property">A property that the assigned method depends on.</param>
+        public OnPropertyChangedAttribute(string property)
+        {
+        }
+    }
+}
