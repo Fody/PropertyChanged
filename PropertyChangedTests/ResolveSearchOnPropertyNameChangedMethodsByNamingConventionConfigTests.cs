2fb1da0fc383d76aae66ffa92833739b46f2c752
TDenis <dtimukhin@yandex.ru>
2015-12-06 23:14:09 +0300

    Add SearchOnPropertyNameChangedMethodsByNamingConvention configuration tests

---
+++ PropertyChangedTests/ResolveSearchOnPropertyNameChangedMethodsByNamingConventionConfigTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Linq;
+using NUnit.Framework;
+
+[TestFixture]
+public class ResolveSearchOnPropertyNameChangedMethodsByNamingConventionConfigTests
+{
+    [Test]
+    public void False()
+    {
+        var xElement = XElement.Parse("<PropertyChanged SearchOnPropertyNameChangedMethodsByNamingConvention='false'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveSearchOnPropertyNameChangedMethodsByNamingConventionConfig();
+        Assert.IsFalse(moduleWeaver.SearchOnPropertyNameChangedMethodsByNamingConvention);
+    }
+
+    [Test]
+    public void True()
+    {
+        var xElement = XElement.Parse("<PropertyChanged SearchOnPropertyNameChangedMethodsByNamingConvention='true'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveSearchOnPropertyNameChangedMethodsByNamingConventionConfig();
+        Assert.IsTrue(moduleWeaver.SearchOnPropertyNameChangedMethodsByNamingConvention);
+    }
+
+    [Test]
+    public void Default()
+    {
+        var moduleWeaver = new ModuleWeaver();
+        moduleWeaver.ResolveSearchOnPropertyNameChangedMethodsByNamingConventionConfig();
+        Assert.IsTrue(moduleWeaver.SearchOnPropertyNameChangedMethodsByNamingConvention);
+    }
+
+}
