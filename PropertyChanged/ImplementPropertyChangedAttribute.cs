a0aa53a49c529e21cf43259436d7f29b47d8d616
Simon Cropp <simon.cropp@gmail.com>
2017-05-16 12:56:11 +1000

    redundant comment

--- PropertyChanged/ImplementPropertyChangedAttribute.cs
+++ PropertyChanged/ImplementPropertyChangedAttribute.cs
@@ -9,9 +9,6 @@ namespace PropertyChanged
     /// PropertyChanged.Fody will weave the <see cref="INotifyPropertyChanged"/> interface and implementation into the class.
     /// When the value of a property changes, the PropertyChanged notification will be raised automatically
     /// </para>
-    /// <para>
-    /// see https://github.com/Fody/PropertyChanged <see href="https://github.com/Fody/PropertyChanged">(link)</see> for more information.
-    /// </para>
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false)]
     public class ImplementPropertyChangedAttribute : Attribute
