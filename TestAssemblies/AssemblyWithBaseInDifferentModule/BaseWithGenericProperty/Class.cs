474278cbaf7ba6d0a70b5289dd0340000184c957
Johannes Hartmann <der_hoschi@gmx.at>
2017-09-25 03:39:57 +0200

    Fix #259: Exception for properties referencing a generic type with equality comparers in another assembly (#283)
    * Added tests for the case where a class has a generic property from another assembly. (#259)
    
    * Fixed error when a generic property with an equality comparer was declared in another assembly (#259)

---
+++ TestAssemblies/AssemblyWithBaseInDifferentModule/BaseWithGenericProperty/Class.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using AssemblyWithBase.BaseWithEquals;
+
+namespace AssemblyWithBaseInDifferentModule.BaseWithGenericProperty
+{
+    public class Class : INotifyPropertyChanged
+    {
+        private string property1;
+
+        public string Property1
+        {
+            get => property1;
+            set
+            {
+                property1 = value;
+                Property2 = new BaseClass1<int>();
+            }
+        }
+
+        public BaseClass1<int> Property2 { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
