79b0bb45b2b4d954c97b49495ca4d83ebb518d86
Simon Cropp <simon.cropp@gmail.com>
2017-11-17 14:13:09 +1100

    .

--- PropertyChangedTests/Verifier.cs
+++ PropertyChangedTests/Verifier.cs
@@ -1,52 +1,54 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using Microsoft.Build.Utilities;
+using NUnit.Framework;
 
 public static class Verifier
 {
-    static string exePath;
-    static bool peverifyFound;
-
-    static Verifier()
-    {
-        var sdkPath = Path.GetFullPath(Path.Combine(ToolLocationHelper.GetPathToDotNetFrameworkSdk(TargetDotNetFrameworkVersion.VersionLatest), "..\\.."));
-        exePath = Directory.GetFiles(sdkPath, "peverify.exe", SearchOption.AllDirectories).LastOrDefault();
-
-        peverifyFound = File.Exists(exePath);
-        if (!peverifyFound)
-        {
-#if(!DEBUG)
-            throw new System.Exception("Could not find PEVerify");
-#endif
-        }
-    }
     public static void Verify(string beforeAssemblyPath, string afterAssemblyPath)
     {
-        if (!peverifyFound)
-        {
-            return;
-        }
-        Debug.WriteLine(afterAssemblyPath);
         var before = Validate(beforeAssemblyPath);
         var after = Validate(afterAssemblyPath);
         var message = $"Failed processing {Path.GetFileName(afterAssemblyPath)}\r\n{after}";
-        NUnit.Framework.Assert.AreEqual(TrimLineNumbers(before), TrimLineNumbers(after), message);
+        Assert.AreEqual(TrimLineNumbers(before), TrimLineNumbers(after), message);
     }
 
-    public static string Validate(string assemblyPath2)
+    static string Validate(string assemblyPath2)
     {
-
-        var process = Process.Start(new ProcessStartInfo(exePath, "\"" + assemblyPath2 + "\"")
+        var exePath = GetPathToPeVerify();
+        if (!File.Exists(exePath))
+        {
+            return string.Empty;
+        }
+        using (var process = Process.Start(new ProcessStartInfo(exePath, "\"" + assemblyPath2 + "\"")
         {
             RedirectStandardOutput = true,
             UseShellExecute = false,
             CreateNoWindow = true
-        });
+        }))
+        {
+            process.WaitForExit(10000);
+            return process.StandardOutput.ReadToEnd().Trim().Replace(assemblyPath2, "");
+        }
+    }
 
-        process.WaitForExit(10000);
-        return process.StandardOutput.ReadToEnd().Trim().Replace(assemblyPath2, "");
+    static string GetPathToPeVerify()
+    {
+        var windowsSdk = Environment.ExpandEnvironmentVariables(@"%programfiles(x86)%\Microsoft SDKs\Windows\");
+        var exePath = Directory.EnumerateFiles(windowsSdk, "PEVerify.exe",SearchOption.AllDirectories)
+            .OrderBy(x =>
+            {
+                var fileVersionInfo = FileVersionInfo.GetVersionInfo(x);
+                return new Version(fileVersionInfo.FileMajorPart, fileVersionInfo.FileMinorPart, fileVersionInfo.FileBuildPart);
+            })
+            .FirstOrDefault();
+        if (exePath == null)
+        {
+            throw new Exception("Could not find path to PEVerify");
+        }
+        return exePath;
     }
 
     static string TrimLineNumbers(string foo)
