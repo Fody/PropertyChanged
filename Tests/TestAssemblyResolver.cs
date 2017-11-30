969b6aa7be3a0ae34cf6047bbef50ef9ce970259
Simon Cropp <simon.cropp@gmail.com>
2017-11-30 18:22:33 +1100

    fix resolution

--- Tests/TestAssemblyResolver.cs
+++ Tests/TestAssemblyResolver.cs
@@ -7,4 +7,25 @@ public class TestAssemblyResolver : DefaultAssemblyResolver
     {
         AddSearchDirectory(TestContext.CurrentContext.TestDirectory);
     }
+
+    public override AssemblyDefinition Resolve(AssemblyNameReference name)
+    {
+#if (NETCOREAPP2_0)
+        if (name.Name == "netstandard")
+        {
+            var netstandard = System.IO.Path.Combine(
+                System.Environment.GetFolderPath(System.Environment.SpecialFolder.ProgramFiles),
+                @"dotnet\sdk\NuGetFallbackFolder\netstandard.library\2.0.0\build\netstandard2.0\ref\netstandard.dll");
+            return AssemblyDefinition.ReadAssembly(
+                fileName: netstandard,
+                parameters: new ReaderParameters(ReadingMode.Deferred)
+                {
+                    AssemblyResolver = this
+                });
+        }
+#endif
+
+        return base.Resolve(name);
+    }
+
 }
