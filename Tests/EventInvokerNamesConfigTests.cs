d9f6ceabad77df558377686264071a2174c1bff5
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-04-05 11:54:22 +0200

    Make the EventInvokerNames config option override the default values

--- Tests/EventInvokerNamesConfigTests.cs
+++ Tests/EventInvokerNamesConfigTests.cs
@@ -7,10 +7,13 @@ public class EventInvokerNamesConfigTests
     public void GetStringComparisonFromXml()
     {
         var xElement = XElement.Parse("<PropertyChanged EventInvokerNames='A,B'/>");
-        var moduleWeaver = new ModuleWeaver {Config = xElement};
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
         moduleWeaver.ResolveEventInvokerName();
         Assert.Contains("A", moduleWeaver.EventInvokerNames);
         Assert.Contains("B", moduleWeaver.EventInvokerNames);
+
+        // Custom values should override the defaults
+        Assert.DoesNotContain("OnPropertyChanged", moduleWeaver.EventInvokerNames);
     }
 
     [Fact]
