3ef214eaed902ffdf15a13d63fd997e579b18e03
Simon Cropp <simon.cropp@gmail.com>
2018-06-10 11:19:57 +1000

    Delete CommonAssemblyInfo.cs

--- CommonAssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("PropertyChanged")]
-[assembly: AssemblyProduct("PropertyChanged")]
-[assembly: AssemblyCompany("Simon Cropp and Contributors")]
-[assembly: AssemblyDescription("Fody add-in for injecting INotifyPropertyChanged code into properties.")]
-[assembly: AssemblyVersion("2.2.4")]
