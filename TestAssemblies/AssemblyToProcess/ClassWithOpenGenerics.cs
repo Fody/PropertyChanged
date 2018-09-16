06c60d2aa742c2c38063cc965c3739ab73cdfea5
tom-englert <mail@tom-englert.de>
2018-09-16 02:40:21 +0200

    Extend tests to ensure we don't have a similar issue like #356 with reference types (#358)

---
+++ TestAssemblies/AssemblyToProcess/ClassWithOpenGenerics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+using PropertyChanged;
+
+[AddINotifyPropertyChangedInterface]
+public class ClassWithOpenGenerics<T>
+{
+    public KeyValuePair<string, T> Property1 { get; set; }
+
+    public Tuple<string, T> Property2 { get; set; }
+}
