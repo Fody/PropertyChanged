21fed066aa46bd5bacc5c1eac46d8f1a2994d177
tom-englert <mail@tom-englert.de>
2018-11-25 11:53:57 +0100

    Add .xcf (#369)

--- Tests/CheckForEqualityConfigTests.cs
+++ Tests/CheckForEqualityConfigTests.cs
@@ -13,9 +13,27 @@ public class CheckForEqualityConfigTests
     }
 
     [Fact]
+    public void False0()
+    {
+        var xElement = XElement.Parse("<PropertyChanged CheckForEquality='0'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveCheckForEqualityConfig();
+        Assert.False(moduleWeaver.CheckForEquality);
+    }
+
+    [Fact]
     public void True()
     {
-        var xElement = XElement.Parse("<PropertyChanged CheckForEquality='true'/>");
+        var xElement = XElement.Parse("<PropertyChanged CheckForEquality='True'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveCheckForEqualityConfig();
+        Assert.True(moduleWeaver.CheckForEquality);
+    }
+
+    [Fact]
+    public void True1()
+    {
+        var xElement = XElement.Parse("<PropertyChanged CheckForEquality='1'/>");
         var moduleWeaver = new ModuleWeaver { Config = xElement };
         moduleWeaver.ResolveCheckForEqualityConfig();
         Assert.True(moduleWeaver.CheckForEquality);
