fbfb4feed363b150b18bcba57935bf9e729ed1b9
Simon Cropp <simon.cropp@gmail.com>
2019-04-28 20:36:35 +1000

    Update EventArgsCache.cs

--- PropertyChanged.Fody/EventArgsCache.cs
+++ PropertyChanged.Fody/EventArgsCache.cs
@@ -9,7 +9,9 @@ public class EventArgsCache
     {
         this.moduleWeaver = moduleWeaver;
         var attributes = TypeAttributes.AutoClass | TypeAttributes.AutoLayout | TypeAttributes.Abstract | TypeAttributes.Sealed | TypeAttributes.BeforeFieldInit | TypeAttributes.Class | TypeAttributes.NotPublic;
-        cacheTypeDefinition = new TypeDefinition(moduleWeaver.ModuleDefinition.Name, "<>PropertyChangedEventArgs", attributes, moduleWeaver.TypeSystem.ObjectReference);
+        // need to qualify the namespace to avoid linker issues
+        var nameSpace = moduleWeaver.ModuleDefinition.Name;
+        cacheTypeDefinition = new TypeDefinition(nameSpace, "<>PropertyChangedEventArgs", attributes, moduleWeaver.TypeSystem.ObjectReference);
     }
 
     public FieldReference GetEventArgsField(string propertyName)
