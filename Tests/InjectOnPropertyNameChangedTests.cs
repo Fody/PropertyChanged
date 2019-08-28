18c62e00b0a90de9fde7f2cc69ee0244f84d71f5
Fozzy <stfos@DFDS.COM>
2019-08-28 12:39:33 +0100

    Moved InjectOnPropertyNameChangedTests into sepearate file. Changed InjectOnPropertyNameChanged to be set directly via property during initialization as opposed to passing in XML config element.

---
+++ Tests/InjectOnPropertyNameChangedTests.cs
@@ -0,0 +1,26 @@
+﻿using Fody;
+using Xunit;
+
+public class InjectOnPropertyNameChangedTests
+{
+    [Fact]
+    public void ModuleWeaver_WhenInjectOnPropertyNameChangedIsTrue_ThrowsWeavingException()
+    {
+        var moduleWeaver = new ModuleWeaver { InjectOnPropertyNameChanged = true };
+
+        var weavingException = Assert.Throws<WeavingException>(() =>
+        {
+            moduleWeaver.ExecuteTestRun("AssemblyWithNonVoidOnPropertyNameChanged.dll");
+        });
+
+        Assert.Equal("The type ClassWithNonVoidOnPropertyChanged has a On_PropertyName_Changed method (OnProperty1Changed) that has a non void return value. Ensure the return type void.", weavingException.Message);
+    }
+
+    [Fact]
+    public void ModuleWeaver_WhenInjectOnPropertyNameChangedIsFalse_DoesNotThrowWeavingException()
+    {
+        var moduleWeaver = new ModuleWeaver { InjectOnPropertyNameChanged = false };
+
+        moduleWeaver.ExecuteTestRun("AssemblyWithNonVoidOnPropertyNameChanged.dll");
+    }
+}
