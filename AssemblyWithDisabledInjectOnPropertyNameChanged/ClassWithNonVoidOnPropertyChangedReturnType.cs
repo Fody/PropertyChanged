3d23b648aaa300e1d1ec1993bb16f938591f332d
Fozzy <stfos@DFDS.COM>
2019-08-28 09:08:08 +0100

    Added a couple of tests to verify InjectOnPropertyNameChanged = false prevents the ModuleWeaver from checking the state of On_PropertyName_Changed methods.

---
+++ AssemblyWithDisabledInjectOnPropertyNameChanged/ClassWithNonVoidOnPropertyChangedReturnType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace AssemblyWithDisabledInjectOnPropertyNameChanged
+{
+    public class ClassWithNonVoidOnPropertyChangedReturnType : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int MyProperty { get; set; }
+
+        private int OnMyPropertyChanged()
+        {
+            return 1;
+        }
+    }
+}
--- Tests/OnPropertyNameChangedConfigTests.cs
+++ Tests/OnPropertyNameChangedConfigTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using Fody;
+using System.Xml.Linq;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -29,6 +30,31 @@ public class OnPropertyNameChangedConfigTests :
         var moduleWeaver = new ModuleWeaver();
         moduleWeaver.ResolveOnPropertyNameChangedConfig();
         Assert.True(moduleWeaver.InjectOnPropertyNameChanged);
+    }
+
+    [Fact]
+    public void ModuleWeaver_WhenInjectOnPropertyNameChangedIsTrue_ThrowsWeavingException()
+    {
+        var xElement = XElement.Parse("<PropertyChanged InjectOnPropertyNameChanged='true'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveOnPropertyNameChangedConfig();
+
+        var weavingException = Assert.Throws<WeavingException>(() =>
+        {
+            moduleWeaver.ExecuteTestRun("AssemblyWithNonVoidOnPropertyNameChanged.dll");
+        });
+
+        Assert.Equal("The type ClassWithNonVoidOnPropertyChanged has a On_PropertyName_Changed method (OnProperty1Changed) that has a non void return value. Ensure the return type void.", weavingException.Message);
+    }
+
+    [Fact]
+    public void ModuleWeaver_WhenInjectOnPropertyNameChangedIsFalse_DoesNotThrowWeavingException()
+    {
+        var xElement = XElement.Parse("<PropertyChanged InjectOnPropertyNameChanged='false'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveOnPropertyNameChangedConfig();
+
+        moduleWeaver.ExecuteTestRun("AssemblyWithNonVoidOnPropertyNameChanged.dll");
     }
 
     public OnPropertyNameChangedConfigTests(ITestOutputHelper output) :
