3b1373e542b7dc4da1368b84176783f604bcae94
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2019-12-12 22:38:46 +0100

    Add a failing test case when the generic base is from a different assembly.

---
+++ TestAssemblies/AssemblyWithBase/GenericMiddle/BaseClassWithGenericMiddleBase.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel;
+
+public class BaseClassWithGenericMiddleBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+    
+    public virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
