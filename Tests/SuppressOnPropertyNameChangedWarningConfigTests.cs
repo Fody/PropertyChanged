7e4253d8d24381ce8d1a04b3bc7269a4d7790249
Cameron MacFarland <cameron@mobility.com.au>
2020-03-11 14:20:57 +0800

    Added SuppressOnPropertyNameChangedWarningConfig

---
+++ Tests/SuppressOnPropertyNameChangedWarningConfigTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Xml.Linq;
+using VerifyXunit;
+using Xunit;
+using Xunit.Abstractions;
+
+public class SuppressOnPropertyNameChangedWarningConfigTests :
+    VerifyBase
+{
+    [Fact]
+    public void False()
+    {
+        var xElement = XElement.Parse("<PropertyChanged SuppressOnPropertyNameChangedWarning='false'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveSuppressOnPropertyNameChangedWarningConfig();
+        Assert.False(moduleWeaver.SuppressOnPropertyNameChangedWarning);
+    }
+
+    [Fact]
+    public void False0()
+    {
+        var xElement = XElement.Parse("<PropertyChanged SuppressOnPropertyNameChangedWarning='0'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveSuppressOnPropertyNameChangedWarningConfig();
+        Assert.False(moduleWeaver.SuppressOnPropertyNameChangedWarning);
+    }
+
+    [Fact]
+    public void True()
+    {
+        var xElement = XElement.Parse("<PropertyChanged SuppressOnPropertyNameChangedWarning='True'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveSuppressOnPropertyNameChangedWarningConfig();
+        Assert.True(moduleWeaver.SuppressOnPropertyNameChangedWarning);
+    }
+
+    [Fact]
+    public void True1()
+    {
+        var xElement = XElement.Parse("<PropertyChanged SuppressOnPropertyNameChangedWarning='1'/>");
+        var moduleWeaver = new ModuleWeaver { Config = xElement };
+        moduleWeaver.ResolveSuppressOnPropertyNameChangedWarningConfig();
+        Assert.True(moduleWeaver.SuppressOnPropertyNameChangedWarning);
+    }
+
+    [Fact]
+    public void Default()
+    {
+        var moduleWeaver = new ModuleWeaver();
+        moduleWeaver.ResolveSuppressOnPropertyNameChangedWarningConfig();
+        Assert.False(moduleWeaver.SuppressOnPropertyNameChangedWarning);
+    }
+
+    public SuppressOnPropertyNameChangedWarningConfigTests(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+}
