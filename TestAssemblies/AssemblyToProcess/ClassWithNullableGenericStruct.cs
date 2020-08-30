ce2d6d20b08043c2239aba5467965d5ba85e23b0
Victor Irzak <6209775+virzak@users.noreply.github.com>
2020-08-30 07:41:37 -0400

    Add a failing test with a nullable generic struct (#624)

---
+++ TestAssemblies/AssemblyToProcess/ClassWithNullableGenericStruct.cs
@@ -0,0 +1,12 @@
+﻿using PropertyChanged;
+
+[AddINotifyPropertyChangedInterface]
+public class ParentOfNullableGenericStruct<T>
+{
+    public GenericStruct<T>? ChildProperty { get; set; }
+}
+
+public struct GenericStruct<T>
+{
+    public T Value { get; }
+}
