c3a3027075306d01aa29379af93561526134be73
tom-englert <mail@tom-englert.de>
2020-10-07 17:54:23 +0200

    Fix #653, #635: Part 1 - Add failing test

---
+++ TestAssemblies/AssemblyWithInheritance/Classes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+public class BaseClass : INotifyPropertyChanged
+{
+    public IList<string> ChangedEvents = new List<string>();
+
+    public virtual int Property1 { get; set; }
+    public int Property2 => Property1 + 1;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        ChangedEvents.Add(propertyName);
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+public class DerivedClass : BaseClass
+{
+    public override int Property1
+    {
+        get => base.Property1;
+        set => base.Property1 = value;
+    }
+
+    public int Property3 => Property1 - 1;
+}
---
+++ Tests/AssemblyWithInheritanceTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests
+{
+    using System.Collections.Generic;
+
+    using Fody;
+
+    using Xunit;
+
+    public class AssemblyWithInheritanceTests
+    {
+        static AssemblyWithInheritanceTests()
+        {
+            var weavingTask = new ModuleWeaver();
+            testResult = weavingTask.ExecuteTestRun("AssemblyWithInheritance.dll");
+        }
+
+        [Fact]
+        public void DerivedClassRaisesAllExpectedEventsAndNoDuplicates()
+        {
+            var instance = testResult.GetInstance("DerivedClass");
+            instance.Property1 = 42;
+
+            var expected = new[] { "Property2", "Property1", "Property3" };
+
+            Assert.Equal(expected, (IList<string>)instance.ChangedEvents);
+        }
+
+        static TestResult testResult;
+    }
+}
