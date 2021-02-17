b188f91580bed62308f8bd032e077a57705d5a18
Cameron <cameron_elliot@yahoo.co.uk>
2021-02-17 23:12:01 +0000

    Latest review items addressed. Added ClassWithWarnings to test more specific warnings messages as to why an OnPropertyChanged() method is not called.

---
+++ TestAssemblies/AssemblyToProcess/ClassWithWarnings.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using PropertyChanged;
+
+
+[AddINotifyPropertyChangedInterface]
+public class ClassWithWarningsBase
+{
+    public string BaseClassProperty { get; set; }
+}
+
+[AddINotifyPropertyChangedInterface]
+public class ClassWithWarnings : ClassWithWarningsBase
+{
+
+    [DoNotNotify]
+    public string Property1 { get; set; }
+
+    [OnChangedMethod(nameof(CallThisInstead))]
+    public string Property2 { get; set; }
+
+    public bool Property1ChangedCalled;
+    public bool Property2ChangedCalled;
+    public bool PropertyXChangedCalled;
+    public bool CallThisInsteadCalled;
+
+
+    void OnProperty1Changed() => Trace.WriteLine("Ignored");
+
+    void OnProperty2Changed() => Trace.WriteLine("Ignored");
+
+    void OnPropertyXChanged() => Trace.WriteLine("Ignored");
+    void OnBaseClassPropertyChanged() => Trace.WriteLine("Ignored");
+
+    void CallThisInstead() => Trace.WriteLine("Ignored");
+}
