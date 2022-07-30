dd59f85bc9852189f158938a29f0233fccf5a616
tom-englert <mail@tom-englert.de>
2022-07-30 14:53:30 +0200

    Some optimization as suggested by @ltrzesniewski

--- PropertyChanged.Fody.Analyzer/ExtensionMethods.cs
+++ PropertyChanged.Fody.Analyzer/ExtensionMethods.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.CodeAnalysis;
+﻿global using static ExtensionMethods;
+
+using System.Diagnostics;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 static class ExtensionMethods
@@ -16,5 +19,11 @@ static class ExtensionMethods
     public static string? NullIfEmpty(this string? value)
     {
         return string.IsNullOrEmpty(value) ? null : value;
+    }
+
+    [Conditional("DEBUG")]
+    public static void Log(string message)
+    {
+        // File.AppendAllText(@"c:\temp\generator.log", $"{DateTime.Now}: {message}\r\n");
     }
 }
