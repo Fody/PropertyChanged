eee4974cb1ec9ab140d9351f6ed9f9ba46f82d01
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2022-09-07 20:53:55 +0200

    Don't raise error for redundant attribute with generated code

---
+++ TestAssemblies/AssemblyToProcess/ClassWithGeneratedPropertyChanged.cs
@@ -0,0 +1,16 @@
+﻿using System.CodeDom.Compiler;
+using System.ComponentModel;
+using PropertyChanged;
+
+public class ClassWithGeneratedPropertyChangedBase
+{
+}
+
+[AddINotifyPropertyChangedInterface]
+public class ClassWithGeneratedPropertyChanged : ClassWithGeneratedPropertyChangedBase, INotifyPropertyChanged
+{
+    public string Property1 { get; set; }
+
+    [GeneratedCode("PropertyChanged.Fody", "TEST")]
+    public event PropertyChangedEventHandler PropertyChanged;
+}
