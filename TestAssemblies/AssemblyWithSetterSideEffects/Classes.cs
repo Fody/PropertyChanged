ba466a78ede711e262fa27c62600c5a1a954d771
Juraj Ahel <juraj.ahel@gmail.com>
2025-02-07 16:26:22 +0100

    add use case of class with a setter with a backing field that has custom code side effects (before or after value assignment)

---
+++ TestAssemblies/AssemblyWithSetterSideEffects/Classes.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+
+public class WithSideEffectBeforeValueAssignment :
+    INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new(propertyName));
+    }
+
+    string _property1;
+    public string Property1
+    {
+        get => _property1;
+        set
+        {
+            CallSideEffectBefore();
+            _property1 = value;
+        }
+    }
+
+    
+    public int SideEffectBeforeCallCount { get; set; }
+    void CallSideEffectBefore() => SideEffectBeforeCallCount++;
+}
+
+public class WithSideEffectAfterValueAssignment :
+    INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new(propertyName));
+    }
+
+
+
+    string _property1;
+    public string Property1
+    {
+        get => _property1;
+        set
+        {
+            _property1 = value;
+            CallSideEffectAfter();
+        }
+    }
+
+    public int SideEffectAfterCallCount { get; set; }
+    void CallSideEffectAfter() => SideEffectAfterCallCount++;
+}
---
+++ Tests/AssemblyWithSetterSideEffectsTests.cs
@@ -0,0 +1,53 @@
+﻿public class AssemblyWithSetterSideEffectsTests
+{
+    static readonly string[] peVerifyIgnoreCodes =
+    {
+#if NETCOREAPP
+        "0x80131869"
+#endif
+    };
+
+    const string assemblyName = "AssemblyWithSetterSideEffects.dll";
+    
+    [Theory]
+    [InlineData([true, "same", "same", 2])]
+    [InlineData([true, "different1", "different2", 2])]
+    [InlineData([false, "same", "same", 2])]
+    [InlineData([false, "different1", "different2", 2])]
+    public void CallsPreAssignmentSideEffect(bool checkForEquality, string firstAssignment, string secondAssignment, int expectedCallCount)
+    {
+        const string className = "WithSideEffectBeforeValueAssignment";
+
+        var weaver = new ModuleWeaver(){CheckForEquality = checkForEquality};
+        var testResult = weaver.ExecuteTestRun(assemblyName, ignoreCodes: peVerifyIgnoreCodes);
+        
+        var instance = testResult.GetInstance(className);
+
+        instance.Property1 = firstAssignment;
+        instance.Property1 = secondAssignment;
+        
+        var callCount = (int)instance.SideEffectBeforeCallCount;
+        Assert.Equal(expectedCallCount, callCount);
+    }
+
+    [Theory]
+    [InlineData([true, "same", "same", 2])]
+    [InlineData([true, "different1", "different2", 2])]
+    [InlineData([false, "same", "same", 2])]
+    [InlineData([false, "different1", "different2", 2])]
+    public void CallsPostAssignmentSideEffect(bool checkForEquality, string firstAssignment, string secondAssignment, int expectedCallCount)
+    {
+        const string className = "WithSideEffectAfterValueAssignment";
+
+        var weaver = new ModuleWeaver() { CheckForEquality = checkForEquality };
+        var testResult = weaver.ExecuteTestRun(assemblyName, ignoreCodes: peVerifyIgnoreCodes);
+        
+        var instance = testResult.GetInstance(className);
+        
+        instance.Property1 = firstAssignment;
+        instance.Property1 = secondAssignment;
+        
+        var callCount = (int)instance.SideEffectAfterCallCount;
+        Assert.Equal(expectedCallCount, callCount);
+    }
+}
